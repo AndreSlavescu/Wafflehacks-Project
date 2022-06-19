f154c3b75ec450fc4335c06876a22af216d17d74
Faren03 <94649061+Faren03@users.noreply.github.com>
2022-06-19 09:11:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from flask import Flask
 from dalle import 
+import pprint 
+import sys 
+import spotipy 
+import spotipy.oauth2 as oauth2 
+import configparser 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+scope = 'user-library-modify'
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+client_id = config.get('SPOTIFY', 'CLIENT_ID')
+client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
+
+
+auth = oauth2.SpotifyClientCredentials(
+    client_id=client_id,
+    client_secret=client_secret
+)
+
+token = auth.get_access_token()
+spotify = spotipy.Spotify(auth=token)
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+
+
 
 app = Flask(__name__)
 
@@ -10,3 +39,24 @@ def get_song():
 
 if __name__ == '__main__':
     app.run()
+
+def playlist_tracks(): 
+    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+    track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+    return playlist_tracks
+    
+def get_playlist():
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
+    return playlists 
+
